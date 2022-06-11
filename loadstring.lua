0f1a61822e1339102d11b6cb953145d11563c55a
Vampira <73371267+7GrandLittleBrother@users.noreply.github.com>
2022-06-11 19:32:53 +0800

    Update loadstring.lua

--- loadstring.lua
+++ loadstring.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/7GrandLittleBrother/SytroNight4ROBLOX/main/scripts/bedwars.lua", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/7GrandLittleBrother/SytroNight4ROBLOX/main/scripts/sn.lua", true))()
