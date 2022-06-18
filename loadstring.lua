fa4526e3920c76245a417a5cf3a178cc92fe368b
NTDCore <102583640+NTDCore@users.noreply.github.com>
2022-06-18 15:27:08 +0700

    Update loadstring.lua

--- loadstring.lua
+++ loadstring.lua
@@ -1,1 +1,3 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/7GrandLittleBrother/SytroNight4ROBLOX/main/scripts/sn.lua", true))()
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/NTDCore/SytroNight4ROBLOX/main/scripts/sn.lua", true))()
